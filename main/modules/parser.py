93ce3805c53a888e2eb485563e532582a1a855bf
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-04-23 18:09:36 +0530

    Update parser.py

--- main/modules/parser.py
+++ main/modules/parser.py
@@ -1,40 +1,36 @@
 import asyncio
+from main.modules.schedule import update_schedule
+from main.modules.usschedule import update_schedulex
 from main.modules.utils import status_text
 from main import status
-from main.modules.usschedule import update_schedulex
 from main.modules.db import get_animesdb, get_uploads, save_animedb
 import feedparser
 from main import queue
 from main.inline import button1
 
 def trim_title(title: str):
-    title = title.rsplit(' ', 1)[0]
-    title = title.replace("[Erai-raws] ", "")
-    title = title.replace("Dr. Stone - New World Cour 2", "Dr Stone New World Part 2")
-    title = title.replace("Mahou Tsukai no Yome Season 2 Cour 2", "Mahou Tsukai no Yome Season 2 Part 2")
+    title = title.replace("Go Go Loser Ranger S01E01 We Are Justice The Dragon Keepers 1080p DSNP WEB-DL AAC2.0 H 264-VARYG (Sentai Daishikkaku, Multi-Subs)", "Sentai Daishikkaku - 01")
+    title = title.replace("Go Go Loser Ranger S01E02 Go Fighter D 1080p DSNP WEB-DL AAC2.0 H 264-VARYG (Sentai Daishikkaku, Multi-Subs)", "Sentai Daishikkaku - 02")
+    title = title.replace("Go Go Loser Ranger S01E03 Our Evil Will Bloom Someday 1080p DSNP WEB-DL AAC2.0 H 264-VARYG (Sentai Daishikkaku, Multi-Subs)", "Sentai Daishikkaku - 03")
     title = title.replace("Dead Mount Death Play 2nd Cour", "Dead Mount Death Play Part 2")
-    title = title.replace("Shangri-La Frontier - Kusogee Hunter, Kamige ni Idoman to Su", "Shangri-La Frontier")
-    title = title.replace("Hataraku Maou-sama!! Part 2", "The Devil is a Part-Timer! S2 Part 2")
+    title = title.replace(" (CA)", "")
+    title = title.replace(" (JA)", "")
     title = title.replace("Tian Guan Ci Fu Di Er Ji", "Heaven Official's Blessing S2")
-    title = title.replace("Me-gumi no Daigo - Kyuukoku no Orange", "Megumi no Daigo - Kyuukoku no Orange")
     title = title.replace("(AAC 2.0) ", "")
     ext = ".mkv"
     title = title + ext
     return title
 
-def multi_sub(title: str):
-    subtitle = title.split()[-1] 
-    return subtitle
+
 
 def parse():
-    a = feedparser.parse("https://siftrss.com/f/oyebWJBqN8")
+    a = feedparser.parse("https://nyaa.si/?page=rss&u=varyg1001&q=sentai")
     b = a["entries"]
     data = []    
 
     for i in b:
         item = {}
         item['title'] = trim_title(i['title'])
-        item['subtitle'] = multi_sub(i['title'])
         item['size'] = i['nyaa_size']   
         item['link'] = "magnet:?xt=urn:btih:" + i['nyaa_infohash']
         data.append(item)
@@ -72,9 +68,4 @@ async def auto_parser():
                 queue.append(i["data"])    
                 print("Saved ", i["name"])   
 
-        try:
-            await update_schedulex()
-        except:
-            pass
-
-        await asyncio.sleep(30)
+        await asyncio.sleep(60)
